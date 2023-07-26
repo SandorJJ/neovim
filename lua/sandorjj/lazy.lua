7a85a45ac4e8cccac76182438d0ff727a3b2031f
SandorJJ <sandorjanosjonas@gmail.com>
2023-07-26 11:00:48 -0500

    Moved lualine options to after/plugin/lualine.lua

--- lua/sandorjj/lazy.lua
+++ lua/sandorjj/lazy.lua
@@ -31,15 +31,6 @@ require("lazy").setup({
 	-- Lualine, custom statusline
 	{
 		"nvim-lualine/lualine.nvim",
-		opts = {
-			options = {
-				icons_enabled = false,
-				theme = "onedark",
-				component_separators = "|",
-				section_separators = "",
-
-			},
-		},
 	},
 
 })
