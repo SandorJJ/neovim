35cfe2943ddf2272db639d65108a6ee90228a280
SandorJJ <sandorjanosjonas@gmail.com>
2023-07-26 12:38:21 -0500

    added lsp-zero

--- lua/sandorjj/lazy.lua
+++ lua/sandorjj/lazy.lua
@@ -16,11 +16,12 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	-- Telescope, fuzzy finding
 	-- https://github.com/nvim-telescope/telescope.nvim
-	{ 
-		"nvim-telescope/telescope.nvim", branch = "0.1.x",
+	{
+		"nvim-telescope/telescope.nvim",
+        branch = "0.1.x",
 		dependencies = { "nvim-lua/plenary.nvim" },
 	},
-    
+
     -- Treesitter, syntex highlighting
     -- https://github.com/nvim-treesitter/nvim-treesitter
     {
@@ -36,7 +37,7 @@ require("lazy").setup({
     -- Undotree, undo history
     -- https://github.com/mbbill/undotree
     "mbbill/undotree",
-    
+
     -- Fugitive, git wrapper
     -- https://github.com/tpope/vim-fugitive
     "tpope/vim-fugitive",
@@ -48,4 +49,27 @@ require("lazy").setup({
 	-- Lualine, custom statusline
 	-- https://github.com/nvim-lualine/lualine.nvim
 	"nvim-lualine/lualine.nvim",
+
+    -- LSP Zero, code completion
+    -- https://github.com/VonHeikemen/lsp-zero.nvim/tree/v2.x
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        dependencies = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {                                      -- Optional
+                'williamboman/mason.nvim',
+                build = function()
+                    pcall(vim.api.nvim_command, 'MasonUpdate')
+                end,
+            },
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},     -- Required
+            {'hrsh7th/cmp-nvim-lsp'}, -- Required
+            {'L3MON4D3/LuaSnip'},     -- Required
+        }
+    },
 })
