28941c3136fcd2f3b83783078a89349bca784a46
SandorJJ <sandorjanosjonas@gmail.com>
2025-03-21 21:30:53 -0500

    changed settings and added a few remaps

---
+++ lua/config/remaps.lua
@@ -0,0 +1,12 @@
+-- Clear highlights
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Disable arrow keys
+vim.keymap.set('n', '<left>', '')
+vim.keymap.set('n', '<right>', '')
+vim.keymap.set('n', '<up>', '')
+vim.keymap.set('n', '<down>', '')
+vim.keymap.set('i', '<left>', '')
+vim.keymap.set('i', '<right>', '')
+vim.keymap.set('i', '<up>', '')
+vim.keymap.set('i', '<down>', '')
--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,2 +1,52 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+-- Set map leaders
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Disable netrw banner
+vim.g.netrw_banner = 0
+
+-- Line numbers
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- Tab indenting
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- Highlight search , replace view, and case settings
+vim.opt.incsearch = true
+vim.opt.inccommand = 'split'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- More colors and enable nerd font
+vim.opt.termguicolors = true
+vim.g.have_nerd_font = true
+
+-- Sides, top, bottom
+vim.opt.wrap = false
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = 'yes'
+vim.opt.colorcolumn = '80'
+
+-- Faster update time
+vim.opt.updatetime = 50
+
+-- Disables showing mode
+vim.opt.showmode = false
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Enable whitespace characters
+vim.opt.list = true
+vim.opt.listchars = { trail = '·' }
+
+-- Highlight cursor line
+vim.opt.cursorline = true
+
+-- Ask for confirm if operation fails
+vim.opt.confirm = true
